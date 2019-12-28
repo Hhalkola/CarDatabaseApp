0d80d1e24b95e60efeb8f1dcbd5ab2cdb883db0b
hhalkola <herman.halkola@gmail.com>
2019-12-28 12:36:09 +0200

    Add project files.

---
+++ CarDatabaseApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDatabaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

bcc3845299fc2f36c1577f3864664fc081938610
Hhalkola <56921125+Hhalkola@users.noreply.github.com>
2019-12-28 13:20:46 +0200

    Fixed Method to Check plate number

--- CarDatabaseApp/Car.cs
+++ CarDatabaseApp/Car.cs
@@ -50,7 +50,7 @@ namespace CarDatabaseApp
                 }
                 else if(i == LettersInPlateNumber)
                 {
-                    if (PlateNumber[i] != LettersInPlateNumber)
+                    if (PlateNumber[i] != PlateNumberSeparator)
                     {
                         returnValue = false;
                     }

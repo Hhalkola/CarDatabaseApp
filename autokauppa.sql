9b388fb66966fc399b5841615601e4a1919c4710
Hhalkola <56921125+Hhalkola@users.noreply.github.com>
2020-01-20 15:12:15 +0200

    Update autokauppa.sql

--- autokauppa.sql
+++ autokauppa.sql
@@ -1,20 +1,25 @@
+-- Fixed some formations and upper and lower case letters
+
 CREATE TABLE IF NOT EXISTS cars (
+    platenumber varchar(10) NOT NULL,
     brand varchar(20) NOT NULL,
     model varchar(16) NOT NULL, 
     "year" integer NOT NULL,
-    id_carpower integer NOT NULL,
+    fuelid integer NOT NULL,
     price integer NOT NULL,
-    id_colour integer NOT NULL,
-    "type" varchar(16) NOT NULL,
-    plateNr varchar(10) NOT NULL,
-    PRIMARY KEY (plateNr)
+    colorid integer NOT NULL,
+    PRIMARY KEY (platenumber)
 );
 
-CREATE TABLE IF NOT EXISTS carpower (
-    carpower_id serial NOT NULL,
+CREATE TABLE IF NOT EXISTS fuel (
+    fuelid serial NOT NULL,
     carpower varchar(10) NOT NULL,
-    PRIMARY KEY (carpower_id)
+    PRIMARY KEY (fuelid)
 );
+
+INSERT INTO fuel (fuelid, carpower) VALUES
+(1, 'Gasoline'),
+(2, 'Diesel');
 
 CREATE TABLE IF NOT EXISTS colour (
     colour_id serial NOT NULL,
@@ -22,28 +27,18 @@ CREATE TABLE IF NOT EXISTS colour (
     PRIMARY KEY (colour_id)
 );
 
-INSERT INTO cars (brand, model, "year", id_carpower, price, id_colour, "type", plateNr) VALUES
-('BMW', '330D', 2007, '2', 5000, '1', 'Wagon', 'OYU-809'),
-('Volvo', 'V90', 2010, '1', 10000, '2', 'Wagon', 'ASD-123'),
-('Audi', 'A6', 2000, '2', 3000, '3', 'Sedan', 'IOO-345'),
-('Opel', 'Corsa', 2008, '1', 2000, '5', 'Hatchback', 'CZZ-987'),
-('BMW', '335i', 2005, '2', 5000, '4', 'Wagon', 'VBC-908'),
-('Volvo', 'XC70', 2019, '1', 25000, '6', 'Wagon', 'WER-432');
-
-INSERT INTO carpower (carpower_id, carpower) VALUES
-(default, 'Gasoline'),
-(default, 'Diesel');
-
 INSERT INTO colour (colour_id, colour) VALUES
-(default, 'Black'),
-(default, 'Red'),
-(default, 'Blue'),
-(default, 'Yellow'),
-(default, 'Grey'),
-(default, 'White');
+(1, 'Black'),
+(2, 'Red'),
+(3, 'Blue'),
+(4, 'Yellow'),
+(5, 'Grey'),
+(6, 'White');
 
-
-
-
-
-
+INSERT INTO cars (platenumber, brand, model, "year", fuelid, price, colorid) VALUES
+('OYU809', 'BMW', '330D', 2007, '2', 5000, '1'),
+('ASD123', 'Volvo', 'V90', 2010, '1', 10000, '2'),
+('IOO345', 'Audi', 'A6', 2000, '2', 3000, '3'),
+('CZZ987', 'Opel', 'Corsa', 2008, '1', 2000, '5'),
+('VBC908','BMW', '335i', 2005, '2', 5000, '4'),
+('WER432','Volvo', 'XC70', 2019, '1', 25000, '6');
